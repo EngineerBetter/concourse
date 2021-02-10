d13c69a8d48cc5a58bb97a164a6e33ad484fc1f2
Colin Simmons <5785492+crsimmons@users.noreply.github.com>
2021-02-10 18:12:02 +0000

    Modify autoscale.sh to handle multiple retriable metrics

--- autoscale.sh
+++ autoscale.sh
@@ -1,26 +1,34 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 : ${CONCOURSE_PROMETHEUS_ENDPOINT?"Must specify CONCOURSE_PROMETHEUS_ENDPOINT"}
 : ${TEAMS?"Must specify TEAMS"}
 
-current_metric=$(prom2json $CONCOURSE_PROMETHEUS_ENDPOINT)
-retried_metrics=$(echo $current_metric | jq -c '.[] | select(.name == "concourse_workers_retried_errors").metrics')
-container_metrics=$(echo $current_metric | jq -c '[.[] | select(.name == "concourse_workers_containers").metrics[] | select(.labels.worker | test("concourse-check-worker-.*") | not)]')
+current_metric=$(prom2json "${CONCOURSE_PROMETHEUS_ENDPOINT}")
+retried_metrics=$(echo "${current_metric}" | jq -c '.[] | select(.name == "concourse_workers_retried_errors").metrics')
+container_metrics=$(echo "${current_metric}" | jq -c '[.[] | select(.name == "concourse_workers_containers").metrics[] | select(.labels.worker | test("concourse-check-worker-.*") | not)]')
 
 for team in $TEAMS; do
-  retried_value=$(echo $retried_metrics | jq -r ".[] | select(.labels.team == \"$team\").value")
-  container_value=$(echo $container_metrics | jq -r ".[] | select(.labels.worker | test(\"concourse-worker-$team-.*\")).value")
+  retried_values=$(echo "${retried_metrics}" | jq -r ".[] | select(.labels.team == \"$team\") | \"\(.labels.error_type) \(.value)\"" | sort -k1)
+  container_value=$(echo "${container_metrics}" | jq -r ".[] | select(.labels.worker | test(\"concourse-worker-$team-.*\")).value")
   retried_state_file=${team}-retried.metric
 
-  if [[ $retried_value -gt $(cat $retried_state_file) ]]; then
+  if [[ ! -f "${retried_state_file}" ]]; then
+    echo -n "${retried_values}" > "${retried_state_file}"
+  fi
+
+  if [[ "${retried_values}" != $(cat "${retried_state_file}") ]]; then
     echo "scaling out workers for team $team"
 #    kubectl scale statefulset/concourse-worker-joebloggs --replicas 2
+    echo -n "${retried_values}" > "${retried_state_file}"
+    exit 0
   fi
-  echo -n $retried_value > $retried_state_file
 
   echo "Container value: $container_value"
   if [[ $container_value == 0 ]]; then
     echo "scaling in workers for team $team"
 #    kubectl scale statefulset/concourse-worker-joebloggs --replicas 2
+  exit 0
   fi
 done
